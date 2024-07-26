027b5ef0d95877bc6ab75778b68fca665edb69bc
tomoya-kato-lvgs <tomoya.kato@leverages.jp>
2024-07-26 17:10:48 +0900

    callback, eventlistener

--- 03_js_basic/040_callback/start/main.js
+++ 03_js_basic/040_callback/start/main.js
@@ -1,15 +1,14 @@
-function fn(number) {
-    return number * 2;
-  }
-  
-  console.log(fn(2));
-  console.log(fn(4));
-  
-  const fnArrow = number => {
-    console.log(number);
-    return number * 2;
-  };
-  
-  const fnArrowObj = number => ({ result: number * 2 })
-  
-  console.log(fnArrowObj(2));
+// callback関数は引数に関数が渡ってくる
+function print(callback) {
+  console.log(callback);
+  // 末尾の()は関数を実行している
+  const result = callback();
+  console.log(result);
+}
+
+function fn(number = 3) {
+  return number * 2;
+}
+
+debugger;
+print(fn);
