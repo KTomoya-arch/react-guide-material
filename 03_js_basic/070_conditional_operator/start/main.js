1037014cdc85d583b87b55b468ca9c0d3c464954
tomoya-kato-lvgs <tomoya.kato@leverages.jp>
2024-07-26 17:44:48 +0900

    ３項演算子

--- 03_js_basic/070_conditional_operator/start/main.js
+++ 03_js_basic/070_conditional_operator/start/main.js
@@ -1,17 +1,21 @@
-// 三項演算子（ ? : ）
+// // 三項演算子（ ? : ）
 
-const a = true;
-let resultA;
+// const a = NaN;
+// let resultA = a ? "true" : "false";
 
-if(a) {
-  resultA = "true";
-} else {
-  resultA = "false";
-}
-console.log(resultA);
+// // if (a) {
+// //   resultA = "true";
+// // } else {
+// //   resultA = "false";
+// // }
+// console.log(resultA);
 
 // function getResult() {
 //   return a ? "true" : "false";
 // }
 
 // console.log(getResult());
+
+const a = 10;
+let resultA = a ? 10 : -10;
+console.log(resultA);
