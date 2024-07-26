279c9fc60d3aa0673b9354d6a6ae0fdb90332dac
tomoya-kato-lvgs <tomoya.kato@leverages.jp>
2024-07-26 17:21:01 +0900

    分割代入

--- 03_js_basic/050_destructuring/start/main.js
+++ 03_js_basic/050_destructuring/start/main.js
@@ -1,26 +1,33 @@
-const arry = ["配列1", "配列2", "配列3"];
-console.log(arry[0]);
-console.log(arry[2]);
+// // 分割代入と呼ぶ
+// const [unko, chinko, sums] = ["配列1", "配列2", "配列3"];
+// console.log(unko);
+// console.log(chinko);
+// console.log(sums);
 
-const obj = { x: "オブジェクト1", y: "オブジェクト2", z: "オブジェクト3" };
-console.log(obj.x);
-console.log(obj.y);
+// // オブジェクトの場合を考える、オブジェクトの場合はプロパティに一致をさせる必要がある
+// const { x, y } = {
+//   x: "オブジェクト1",
+//   y: "オブジェクト2",
+//   z: "オブジェクト3",
+// };
+// console.log(x);
+// console.log(y);
 
 const arr = ["Japan", "Tokyo", "Shinjuku"];
 const objAddress = { country: "Japan", state: "Tokyo", city: "Shinjuku" };
 
-const fnArr = (arry) => {
+const fnArr = ([country, state, city]) => {
   console.log("---配列---");
-  console.log(`country: ${arry[0]}`);
-  console.log(`state: ${arry[1]}`);
-  console.log(`city: ${arry[2]}`);
+  console.log(`country: ${country}`);
+  console.log(`state: ${state}`);
+  console.log(`city: ${city}`);
 };
 
-const fnObj = (objAddr) => {
+const fnObj = ({ country, state, city }) => {
   console.log("---オブジェクト---");
-  console.log(`country: ${objAddr.country}`);
-  console.log(`state: ${objAddr.state}`);
-  console.log(`city: ${objAddr.city}`);
+  console.log(`country: ${country}`);
+  console.log(`state: ${state}`);
+  console.log(`city: ${city}`);
 };
 
 fnArr(arr);
