e2c7aa6d4021549e852958374e1f7765132746bd
tomoya-kato-lvgs <kato.eng.dao@gmail.com>
2024-07-28 14:34:38 +0900

    map

--- 06_control_and_form/src/010_list_components/start/Example.jsx
+++ 06_control_and_form/src/010_list_components/start/Example.jsx
@@ -1,14 +1,14 @@
-
 const animals = ["Dog", "Cat", "Rat"];
 
 const Example = () => {
-
+  const animalList = [];
+  for (const animal of animals) {
+    animalList.push(<li>Hello, {animal}</li>);
+  }
   return (
     <>
       <h3>配列の操作</h3>
-      <ul>
-        
-      </ul>
+      <ul>{animalList}</ul>
     </>
   );
 };
